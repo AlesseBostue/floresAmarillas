9136bc271bd145fb2ea827a3730d3b9047017d00
Aless <argo@pop-os.local>
2024-09-22 02:25:54 -0500

    arreglando errores
    se modifico la funcion que genera las imagenes y se cambio el esquema, tambien se le añadio mas decoracion al fondo

--- src/app/bg-flowers/bg-flowers.component.ts
+++ src/app/bg-flowers/bg-flowers.component.ts
@@ -11,16 +11,45 @@ import { MessageComponent } from "../message/message.component";
 })
 export class BgFlowersComponent {
   // flowers = new Array(10).fill(null);
-  flowers: { top: string; left: string; }[] = [];
+  flowers: { url:String; top: string; left: string; }[] = [];
+	sparkles: { top: number; left: number; delay: number }[] = [];
+	numImagenes = 12;
 
   constructor() {
     this.genRandomPosition();
+		this.genSparkles(50); // Genera 50 brillitos
   }
 
+	/* metodos */
   private genRandomPosition() {
+
+		// imagenes a renderizar
+		const imgUrls = [
+			'https://images.vexels.com/media/users/3/185318/isolated/preview/3463339e4c90719310ded8dd95e64689-petalos-elipticos-de-flor-amarilla-planos.png',
+			'https://cdn-icons-png.flaticon.com/512/8696/8696403.png',
+			'https://i.imgur.com/xwF4WpM.png',
+			'https://i.imgur.com/UwiJSoK.png',
+
+		]
+
 		const positions = this.genGridPosition(3, 4);
 		const shufflePosition = this.shufflePositions(positions);
-  	this.flowers = this.selectTopPositions(shufflePosition, 20);
+		const selectedPositions = this.selectTopPositions(shufflePosition, this.numImagenes);
+		const repeatedImageUrls = Array.from({ length: this.numImagenes }, (_, index) => imgUrls[index % imgUrls.length]);
+
+    // Asigna las URLs y posiciones a las imágenes
+    this.flowers = repeatedImageUrls.map((url, index) => ({
+      url: url,
+      top: selectedPositions[index].top,
+      left: selectedPositions[index].left
+    }));
+  }
+
+		// this.flowers = imgUrls.map((url, index) => ({
+    //   url: url,
+    //   top: `${shufflePosition[index].top}%`,
+    //   left: `${shufflePosition[index].left}%`
+    // }));
 
     // for(let i = 0; i < 10; i++){
     //   const top = Math.random() * 90;
@@ -31,7 +60,6 @@ export class BgFlowersComponent {
     //     left: `${left}%`
     //   });
     // }
-  }
 
 	private genGridPosition(rows: number, cols: number) : { top: number; left: number; }[] {
 		const positions = [];
@@ -72,5 +100,16 @@ export class BgFlowersComponent {
 			left: `${pos.left}%`
 		}));
 	}
+
+	// Genera posiciones aleatorias para los brillitos
+  private genSparkles(count: number) {
+    for (let i = 0; i < count; i++) {
+      this.sparkles.push({
+        top: Math.random(),
+        left: Math.random(),
+        delay: Math.random()
+      });
+    }
+  }
 
 }
